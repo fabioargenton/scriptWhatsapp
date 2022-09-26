0da370b0c04ffff9e2959575e2e102162fa6569f
Fabio Eduardo Argenton <fabio.argenton@hotmail.com>
2022-09-26 13:23:32 -0300

    Update
    Correção

--- código.js
+++ código.js
@@ -1,25 +1,32 @@
-var mensagens = 0
-async function enviarScript(texto, loop){
-    mensagens = loop
-    scriptText = texto.split(/[\n\t]+/).map(linha => linha.trim()).filter(linha => linha)
-	main = document.querySelector("#main")
-	textarea = main.querySelector(`div[contenteditable="true"]`)
-
-	if(!textarea) throw new Error("Não há uma conversa aberta")
-
-	for(var i = 0; i < loop; i++){
-		console.log(i + ': ' + scriptText)
-
-		textarea.textContent = scriptText
-		textarea.dispatchEvent(new InputEvent("input", { bubbles: true }));
-
-		(main.querySelector(`[data-testid="send"]`) || main.querySelector(`[data-icon="send"]`)).click()
-		
-		if(scriptText.indexOf(i) !== scriptText.length - 1) await new Promise(resolve => setTimeout(resolve, 250))
-	}
-
-	return scriptText.length
+//Função que digita a mensagem e envia
+async function enviarScript(scriptText){
+	const lines = scriptText.split(/[\n\t]+/).map(line => line.trim()).filter(line => line);
+	main = document.querySelector("#main"),
+	textarea = main.querySelector(`div[contenteditable="true"]`)	
+	if(!textarea) throw new Error("Não há uma conversa aberta")	
+	for(const line of lines){
+        console.log(line)	
+		textarea.focus();
+		document.execCommand('insertText', false, line);
+		textarea.dispatchEvent(new Event('change', {bubbles: true}));	
+		setTimeout(() => {
+			(main.querySelector(`[data-testid="send"]`) || main.querySelector(`[data-icon="send"]`)).click();
+		}, 100);		
+		if(lines.indexOf(line) !== lines.length - 1) await new Promise(resolve => setTimeout(resolve, 250));
+	}	
+	return lines.length;
 }
 
-//Digite a mensagem desejada e a qtde de vezes que ela será enviada para a vitima 
-enviarScript(`Seu texto aqui`,10).then(e => console.log(`Código finalizado, ${mensagens} mensagens enviadas`)).catch(console.error)
+//Função que chama a função enviarScript dentro de um loop
+function spam(message, loop) {
+	var delay = 1000; // 1 second
+	
+	for (var i = 0; i < loop; i++){
+		window.setTimeout(function(){
+			enviarScript(message);
+		}, delay*i);
+	}
+}
+
+//Executa o ataque
+spam("mensagem_aqui", 10); //Vai enviar 10 vezes essa msg
